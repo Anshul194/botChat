f30da59901eb9542ecac32752c916e0b11c2a900
developer-divyang <104240941+developer-divyang@users.noreply.github.com>
2026-05-08 16:34:57 +0530

    changes in repo for etest deploy

--- botchat-app/app/page.tsx
+++ botchat-app/app/page.tsx
@@ -3,5 +3,5 @@ import { redirect } from "next/navigation";
 export default function Home() {
   // Redirect to the landing page instead of the sign-in page
   redirect("/landing");
-  
+  //redirect on landing page 
 }
